3aebe4b2d8719021d1a24f15bbade0e63c52e021
Elena Todd <elenatwhite@gmail.com>
2019-11-08 09:20:08 -0600

    Add project files.

---
+++ LifeTheGreenWay/Models/BlogPost.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LifeTheGreenWay.Models
+{
+    public class BlogPost
+    {
+        public int ID { get; set; }
+        public DateTime Date { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
